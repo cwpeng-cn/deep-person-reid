219b8fa1eec3d7bbb5976a695b69321ffb9d76b7
pengcw <32771262+ThinkerPcw@users.noreply.github.com>
2020-11-23 12:00:13 +0800

    add prid_baseline.py

--- prid_baseline.py
+++ prid_baseline.py
@@ -3,7 +3,7 @@ import torchreid
 
 # Each batch contains batch_size*seq_len images
 datamanager = torchreid.data.VideoDataManager(
-    root='../',
+    root='./',
     sources='prid2011',
     height=256,
     width=128,
