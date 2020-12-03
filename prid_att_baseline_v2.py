a8825213cc5a56f367c90141b17f7b7864234ae5
cwpeng-cn <cw.peng@foxmail.com>
2020-12-03 23:55:08 +0800

    fix prid_att_baseline_v2.py

--- prid_att_baseline_v2.py
+++ prid_att_baseline_v2.py
@@ -27,7 +27,7 @@ model = torchreid.models.build_model(
 load_pretrained_weights(model=model, weight_path="log/resnet50-softmax-prid2011/model/model.pth.tar-43")
 model = model.cuda()
 optimizer = torchreid.optim.build_optimizer(
-    model, optim='adam', lr=0.01, staged_lr=True, new_layers=['fc', 'classifier', "encoder", "attn", "ff"],
+    model, optim='adam', lr=0.01, staged_lr=True, new_layers=['fc', 'classifier', "attention"],
     base_lr_mult=0
 )
 
