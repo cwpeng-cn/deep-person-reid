16f9f4a8e172b0794056c15f1004adc771f4f5fd
pengcw <32771262+ThinkerPcw@users.noreply.github.com>
2020-12-04 10:18:46 +0800

    change parameter in prid_att_baseline_v2.py

--- prid_att_baseline_v2.py
+++ prid_att_baseline_v2.py
@@ -24,11 +24,11 @@ model = torchreid.models.build_model(
     N=2
 )
 
-load_pretrained_weights(model=model, weight_path="log/resnet50-softmax-prid2011/model/model.pth.tar-43")
+# load_pretrained_weights(model=model, weight_path="log/resnet50-softmax-prid2011/model/model.pth.tar-43")
 model = model.cuda()
 optimizer = torchreid.optim.build_optimizer(
     model, optim='adam', lr=0.0003, staged_lr=True, new_layers=['fc', 'classifier', "attention"],
-    base_lr_mult=0.01
+    base_lr_mult=0.1
 )
 
 scheduler = torchreid.optim.build_lr_scheduler(
