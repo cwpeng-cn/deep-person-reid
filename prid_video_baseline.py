1d614949cf6103e9600087777d29e8fb085a9414
cwpeng-cn <cw.peng@foxmail.com>
2020-12-05 20:25:22 +0800

    change parameter in prid_video_baseline.py

--- prid_video_baseline.py
+++ prid_video_baseline.py
@@ -9,8 +9,8 @@ datamanager = torchreid.data.VideoDataManager(
     height=256,
     width=128,
     combineall=False,
-    batch_size_train=8,  # number of tracklets
-    seq_len=15  # number of images in each tracklet
+    batch_size_train=10,  # number of tracklets
+    seq_len=16  # number of images in each tracklet
 )
 
 model = torchreid.models.build_model(
