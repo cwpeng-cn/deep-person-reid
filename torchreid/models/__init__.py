51997f929a7e8e1ff578cc6e5fe7e7b08b9398e2
cwpeng-cn <cw.peng@foxmail.com>
2020-12-05 18:53:40 +0800

    add prid_video_baseline.py and resnet_video.py

--- torchreid/models/__init__.py
+++ torchreid/models/__init__.py
@@ -24,6 +24,7 @@ from .inceptionresnetv2 import *
 from .resnet_selfattention import resnet50_fc512 as resnet50_fc512_att
 from .resnet_selfattention_v2 import resnet50_fc512 as resnet50_fc512_att_v2
 from .resnet_selfattention_v3 import resnet50_fc512 as resnet50_fc512_att_v3
+from .resnet_video import resnet50_fc512 as resnet_video
 
 __model_factory = {
     # image classification models
@@ -77,7 +78,8 @@ __model_factory = {
     'osnet_ain_x1_0': osnet_ain_x1_0,
     'resnet50_fc512_att': resnet50_fc512_att,
     'resnet50_fc512_att_v2': resnet50_fc512_att_v2,
-    'resnet50_fc512_att_v3': resnet50_fc512_att_v3
+    'resnet50_fc512_att_v3': resnet50_fc512_att_v3,
+    'resnet_video': resnet_video
 }
 
 
