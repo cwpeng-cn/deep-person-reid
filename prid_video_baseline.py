16abcca522202d10f2370fcf4e7a3de74b699ae8
cwpeng-cn <cw.peng@foxmail.com>
2020-12-05 19:24:06 +0800

    add prid_video_baseline.py and resnet_video.py

--- prid_video_baseline.py
+++ prid_video_baseline.py
@@ -27,7 +27,7 @@ model = torchreid.models.build_model(
 # load_pretrained_weights(model=model, weight_path="log/resnet50-softmax-prid2011/model/model.pth.tar-43")
 model = model.cuda()
 optimizer = torchreid.optim.build_optimizer(
-    model, optim='adam', lr=1e-4, staged_lr=True, new_layers=['fc', 'classifier', "attention"],
+    model, optim='adam', lr=8e-4, staged_lr=True, new_layers=['fc', 'classifier', "attention"],
     base_lr_mult=0.1
 )
 
