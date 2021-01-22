52e8908b61775f1af220f7f43f410af1f488d93a
“cwpeng-cn” <cw.peng@foxmail.com>
2021-01-22 12:05:29 +0800

    add pose reid

--- prid_pose_video.py
+++ prid_pose_video.py
@@ -12,7 +12,7 @@ datamanager = torchreid.data.VideoDataManager(
     height=256,
     width=128,
     combineall=False,
-    batch_size_train=16,  # number of tracklets
+    batch_size_train=8,  # number of tracklets
     seq_len=15  # number of images in each tracklet
 )
 
